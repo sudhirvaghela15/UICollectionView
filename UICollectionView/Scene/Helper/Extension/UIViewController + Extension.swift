4e80e4805aa6a18c8b9c78abb25ce0dff8ac5114
Sudhir <74654033+sudhirvaghela15@users.noreply.github.com>
2024-06-29 01:10:33 +0530

    added UIViewController extension helper method's

---
+++ UICollectionView/Scene/Helper/Extension/UIViewController + Extension.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController + Extension.swift
+//  UICollectionView
+//
+//  Created by sudhir on 29/06/24.
+//
+
+import UIKit
+
+typealias CallBackVoid = () -> Void
+typealias CallBack<T> = (T) -> Void
+
+extension UIViewController  {
+	static var className: String {
+		return String(describing: self)
+	}
+	
+	func initMessage(file: StaticString = #file) {
+		print("init Class : - \(String.init(describing: self.classForCoder)) \nFile Name :- \(file)")
+	}
+	
+	func deinitMessage(file: StaticString = #file) {
+		print("Deinit Class : - \(String.init(describing: self.classForCoder)) \nFile Name :- \(file)")
+	}
+}
+
+extension UIViewController {
+	/// hide nav bar
+	func setNavigationBar (hidden isHidden: Bool) {
+		self.navigationController?.setNavigationBarHidden(isHidden, animated: false)
+	}
+}
